38241b7b4f978069a56393e4689b635dddc77314
Alevtina <_@email.comgit>
2022-09-30 15:03:13 +0300

    Add recurs real numbers natural ot 1 do n

---
+++ N_natur_ot1doN/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+System.Console.Write("Введите число n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Real(n));
+
+string Real(int num)
+{
+    if (num == 1)
+    return num.ToString();
+    return num.ToString() + Real(num-1);
+}
+
